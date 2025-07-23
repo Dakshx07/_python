4f2c56e9f2c76d5fd0e1ecd30291d9ab7940140a
Daksh Hiran <filetools@gmail.com>
2025-07-23 13:09:27 +0530

    Initial commit

---
+++ ch-1/problem01.py
@@ -0,0 +1,8 @@
+print(""" 
+twinkle twinkle little star
+how I wonder what you are       
+are you gonna buy me a beer
+       or sell me a rock    
+       or give me a puppy   
+       or a kitten  
+""")
