4572daee01829895e277eaf5d10b22f4e35341f0
Daksh Hiran <filetools@gmail.com>
2025-07-26 22:41:04 +0530

    Add negative slicing examples

---
+++ ch-3/negative_slicing.py
@@ -0,0 +1,11 @@
+name="Daksh"
+
+print(name[0:3])
+
+print(name[-4:-1])  #negative slicing
+print(name[1:4])    #make it opposite of negative slicing to understand the concept
+
+print(name[:4])
+
+print(name[1:])
+print(name[1:5]) #is same as above print statement works same as above. [1:] is same as [1:len(name)]
