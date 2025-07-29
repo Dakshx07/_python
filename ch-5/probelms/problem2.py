957e1a5a430bd62aca907fbdc74185aae1172668
Dakshx07 <daksh.hiran@gmail.com>
2025-07-29 22:35:49 +0530

    main

---
+++ ch-5/probelms/problem2.py
@@ -0,0 +1,20 @@
+s=set()
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+numbers=input('eNter 8 nunber of your choice : ')
+s.add(int(numbers))  # Add the input number to the set
+
+
+print(s)  # Print the set after adding all elements
