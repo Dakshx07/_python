fb637622b2fc5498a7f533e90324685ab70ea1b3
Dakshx07 <daksh.hiran@gmail.com>
2025-07-31 17:56:28 +0530

    break, continue, pass statements

---
+++ ch-7/for_break.py
@@ -0,0 +1,11 @@
+for i in range(1,11):
+    if(i == 8):
+        break  # Exit the loop
+    print(i)  # Print numbers from 1 to 10
+
+for i in range(11):
+    if(i == 7):
+        continue  # Skip the number 7
+    print(i)  # Print numbers from 0 to 10, skipping 7
+
+    #break statement is used to exit the loop
---
+++ ch-7/pass.py
@@ -0,0 +1,9 @@
+for i in range(1,11):
+    pass  # Do nothing for numbers 1 to 10
+
+#pass statement is used when a statement is required syntactically but you do not want any command or code to execute
+
+i=0
+while(i < 11):
+    print(i) # Print numbers from 0 to 10
+    i += 1
