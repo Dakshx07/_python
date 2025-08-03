4029051277f4c48ce4d84ff6216199d12c3c8127
Dakshx07 <daksh.hiran@gmail.com>
2025-08-03 12:28:15 +0530

    function avg()

---
+++ ch-8/func.py
@@ -0,0 +1,9 @@
+def avg():
+    a=int(input("Enter a number:"))
+    b=int(input("Enter another number:"))
+    c=int(input("Enter another number:"))
+
+    average=(a+b+c)/3
+    print("The average of the three numbers is",average)
+
+avg()
